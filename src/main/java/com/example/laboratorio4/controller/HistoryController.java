c5a837f893c8339b2b4bb610e663638bf828e68b
Rubi Polanco <a20183238@pucp.edu.pe>
2021-05-03 20:07:26 -0500

    ejercicio 3

--- src/main/java/com/example/laboratorio4/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio4/controller/HistoryController.java
@@ -1,6 +1,8 @@
 package com.example.laboratorio4.controller;
 
+import com.example.laboratorio4.repository.EmployeesRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/history")
 public class HistoryController {
 
+    EmployeesRepository employeesRepository;
+
     @GetMapping(value = {"","/"})
-    public String historialEmpleado(){
-        //COMPLETAR
+    public String historialEmpleado(Model model){
+
+        model.addAttribute("listaHistorial", employeesRepository.empleadosmastiempotrabajando());
+
+        return "history/lista";
     }
 
 
--- src/main/java/com/example/laboratorio4/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio4/repository/EmployeesRepository.java
@@ -1,7 +1,9 @@
 package com.example.laboratorio4.repository;
 
+import com.example.laboratorio4.dto.Empleadosportiempo;
 import com.example.laboratorio4.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,16 @@ import java.util.List;
 public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
 
 
-
+    @Query(value = "select e.first_name, e.last_name, j.job_title, date(jh.start_date) as fecha_inicio, \n" +
+            "date(jh.end_date) as fecha_fin, \n" +
+            "date_format(from_days(datediff(jh.end_date,jh.start_date)),\"%y\") as anios_trabajados,\n" +
+            "date_format(from_days(datediff(jh.end_date,jh.start_date)),\"%m\") as meses_trabajados\n" +
+            "from hr.job_history jh \n" +
+            "inner join hr.employees e \n" +
+            "on e.employee_id = jh.employee_id\n" +
+            "inner join hr.jobs j\n" +
+            "on j.job_id = e.job_id\n" +
+            "order by datediff(jh.end_date,jh.start_date) desc", nativeQuery = true)
+    List<Empleadosportiempo> empleadosmastiempotrabajando();
 
 }
